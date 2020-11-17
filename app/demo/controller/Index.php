9b1a8f55588e989e6177e9362d7c8bfc4ed4d4bc
yundongku <550460713@qq.com>
2020-11-17 08:59:50 +0800

    第一次上传试验

---
+++ app/demo/controller/Index.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\demo\controller;
+
+use app\BaseController;
+
+class Index extends BaseController{
+
+    public function index(){
+        return 123;
+    }
+}
---
+++ config/status.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'not_found_controller' => -99,
+    'not_found_method'     => -100,
+];
